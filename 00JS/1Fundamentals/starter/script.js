c58917df85ffe27bde5bcedb25d0335217b66cf1
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-17 15:52:23 +0800

    lesson: values and variables

--- 00JS/1Fundamentals/starter/script.js
+++ 00JS/1Fundamentals/starter/script.js
@@ -4,3 +4,16 @@ if (js === "amazing") {
 }
 
 console.log(40 + 8 - 10);
+
+// NOTE: (9) VALUES AND VARIABLES
+console.log(49 + 10 + 5 - 10); //? 54
+
+// Declare a variable with an initial value
+// Use camelCase for variable names
+// Do not start with a number or special character
+// Do not use reserved JS keywords
+// Variables are case-sensitive
+// Use descriptive names
+let firstName = "Ethan";
+let age = 24;
+console.log(firstName, age); //? Ethan 24
