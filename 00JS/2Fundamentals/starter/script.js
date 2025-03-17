bcf3e7514c8330dcbd4d2c10daf580208ac365f0
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-17 19:42:42 +0800

    lesson: strict mode

---
+++ 00JS/2Fundamentals/starter/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// NOTE:  Declare variables
+let hasDriversLicense = false;
+const passTest = true;
+
+// NOTE:  If passTest is true, set hasDriverLicense to true
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive!");
