323d2c99bab01a4866db2af547249f8ee0565d5d
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-18 14:41:47 +0800

    lesson: selecting and manipulating elements

--- 00JS/4DOM/05-Guess-My-Number/starter/script.js
+++ 00JS/4DOM/05-Guess-My-Number/starter/script.js
@@ -4,3 +4,19 @@
 
 // get the text content of the element that has the class "message"
 console.log(document.querySelector('.message').textContent);
+
+// DOM is a tree structure
+// It allows JS to access HTML elements and style to manipulate them
+// DOM is not equal to Javascript
+
+// NOTE: (74) Selecting and Manipulating Elements
+console.log('----Selecting and Manipulating Elements----');
+
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent); //now changed (Manipulated the textContent of the p that contains the message class)
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 21;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
