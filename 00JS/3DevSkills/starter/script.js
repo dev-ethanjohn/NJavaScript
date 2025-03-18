707e995bc044c2ecf1e08904726e26be78e363a1
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-18 14:16:16 +0800

    lesson: debugger

--- 00JS/3DevSkills/starter/script.js
+++ 00JS/3DevSkills/starter/script.js
@@ -31,3 +31,18 @@ const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const array2 = [10, 11, 12, 13, 14, 15, 16, 17, 18];
 const array3 = array1.concat(array2);
 console.log(array3); //
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('Degrees celsius:')),
+    // value: 10,
+  };
+  console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+const result = measureKelvin();
+console.log(result);
