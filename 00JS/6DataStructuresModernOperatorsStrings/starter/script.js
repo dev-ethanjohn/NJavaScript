6f2ab8c29b08eadecba2541793ab992878406632
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-20 15:44:42 +0800

    lesson: destructuring objects

--- 00JS/6DataStructuresModernOperatorsStrings/starter/script.js
+++ 00JS/6DataStructuresModernOperatorsStrings/starter/script.js
@@ -24,7 +24,7 @@
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
+  nameR: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
@@ -34,20 +34,33 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  // openingHours: {
-  //   thu: {
-  //     open: 12,
-  //     close: 22,
-  //   },
-  //   fri: {
-  //     open: 11,
-  //     close: 23,
-  //   },
-  //   sat: {
-  //     open: 0, // Open 24 hours
-  //     close: 24,
-  //   },
-  // },
+  // Destucturing the Object immediately.
+  orderDeliver: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    // console.log(obj);
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}` //called at line 140
+    );
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 };
 
 let [main, , secondary] = restaurant.categories;
@@ -76,3 +89,62 @@ console.log(i, j, k); // 2 5 6
 // Setting default values is useful particular when cases we dont know the array like dealing with APIs, where we dont know the data we are getting.
 const [p = 1, q = 1, r = 2] = [8, 9];
 console.log(p, q, r); //? 8 9 2
+
+// (108) Destructuring Objects
+console.log('--- Destructuring Objects ---');
+
+// NOTE: REFER TO LINE 37 above
+const { nameR, openingHours, categories } = restaurant;
+console.log(nameR, openingHours, categories);
+
+// Changing the variable names
+const {
+  nameR: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Setting default values
+// empty array as default value if the property is not present in the object
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = {
+  a: 23,
+  b: 7,
+  c: 14,
+};
+
+({ a, b } = obj); // wrapping in () to avoid syntax error
+// This causes a syntax error because JavaScript interprets { a, b } as a block statement , not an object literal. Inside a block, = is not valid syntax.
+// this works because we are not declaring a new variable but we are assigning a new value to the existing variable
+console.log(a, b); // 23 7
+
+// NESTED OBJECT
+const user = {
+  userName: 'Alice',
+  address: {
+    city: 'New York',
+    zip: 10001,
+  },
+};
+
+const {
+  userName,
+  address: { city, zip },
+} = user;
+
+console.log(userName, city, zip); // Output: Alice New York 10001
+
+// NOTE:
+// passing an object of options
+restaurant.orderDeliver({
+  time: '22:30',
+  address: 'Viva del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+}); //? Order received! Garlic Bread and Risotto will be delivered to Viva del Sole, 21 at 22:30
