933bdffb4fb267a49b4fe8a6fa625552472fdf8a
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-24 17:16:28 +0800

    lesson: forEach with Maps and Sets

--- 00JS/8WorkingWithArrays/starter/script.js
+++ 00JS/8WorkingWithArrays/starter/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -165,4 +165,25 @@ movements.forEach(function (mov, i, arr) {
   } else {
     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`); // converts to absolute value
   }
+});
+
+// IMPORTANT: (150): forEach with Maps and Sets
+console.log('----forEach with Maps and Sets----');
+
+//  *MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// *SET
+const currenciesUnique = new Set(['USD', 'GBP', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
 });
