1de54a8aae7d9877b1e8c1371d71505714b95047
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-26 21:27:27 +0800

    feat: implement sticky nav bar

--- 00JS/10AdvancedDOM/starter/main.js
+++ 00JS/10AdvancedDOM/starter/main.js
@@ -147,3 +147,17 @@ tabsContainer.addEventListener('click', e => {
 const nav = document.querySelector('.nav');
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky Navigation
+
+const initialCoordinates = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function (e) {
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoordinates.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
