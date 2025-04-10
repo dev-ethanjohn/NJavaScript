a71d145346c0b2ce437fbf5b225884e0cca7fc11
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-04-10 11:40:33 +0800

    refactor: add conditional checks to validate fields

--- 00JS/12Mappy/starter/script.js
+++ 00JS/12Mappy/starter/script.js
@@ -11,8 +11,50 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+// let map, mapEvent;
 
+class Workout {
+  date = new Date(); //? returns a Data object
+  id = (Date.now() + '').slice(-10); //? returns a timestamp
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; //in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace(); //call immediately to calculate the pace (no need to return)
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km / hr
+    this.speed = this.distance / (this.duration / 60);
+  }
+}
+
+// const run = new Running([39, -12], 5.2, 24, 178);
+// const cyc1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run, cyc1);
+
+///////////////////////////////////////////////////////////////////////////
+// APP ARCHITECTURE
 class App {
   #map;
   #mapEvent;
@@ -63,19 +105,70 @@ class App {
   }
 
   _newWorkout(e) {
+    // ? Helper method
+    const inputValid = (...inputs) => {
+      return inputs.every(input => Number.isFinite(input));
+    };
+
+    const allPositive = (...inputs) => {
+      return inputs.every(input => input > 0);
+    };
+
+    const notEmpty = (...inputs) => inputs.every(input => input !== '');
+
     e.preventDefault();
 
-    // clear input fields
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
+    //Get data from form
+    const type = inputType.value;
+    const distanceStr = inputDistance.value;
+    const durationStr = inputDuration.value;
+    const cadenceStr = inputCadence.value;
+    const elevationStr = inputElevation.value;
 
-    //* Display the marker
-    const { lat, lng } = this.#mapEvent.latlng;
-    console.log(`You clicked the map at Latitude: ${lat}, Longitude: ${lng}`);
+    const distance = +distanceStr;
+    const duration = +durationStr;
+    // Check if data is valid
 
+    // Check for empty fields first
+    if (
+      (type === 'running' && (!distanceStr || !durationStr || !cadenceStr)) ||
+      (type === 'cycling' && (!distanceStr || !durationStr || !elevationStr))
+    ) {
+      alert('Please fill in all the fields');
+      return;
+    }
+
+    //* If workout running, create running object
+    if (type === 'running') {
+      const cadence = +cadenceStr;
+      // Check if data is valid
+      if (
+        !inputValid(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      ) {
+        alert('Inputs must be a positiive number');
+        inputDistance.value = inputDuration.value = inputCadence.value = '';
+        return;
+      }
+    }
+
+    //* if workout cycling, create cycling obect
+    if (type === 'cycling') {
+      const elevation = +elevationStr;
+      // Check if data is valid (IF FALSE)
+      if (
+        !inputValid(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      ) {
+        alert('Inputs must be a positiive number');
+        inputDistance.value = inputDuration.value = inputElevation.value = '';
+        return;
+      }
+    }
+
+    // add new object to workout array
+
+    // Render workout on map as a marker
     //* Create a new marker at the clicked location
     L.marker([lat, lng])
       .addTo(this.#map)
@@ -90,6 +183,19 @@ class App {
       )
       .setPopupContent('workout')
       .openPopup();
+
+    // Render workout on list
+
+    //Hide form  clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    //* Display the marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    console.log(`You clicked the map at Latitude: ${lat}, Longitude: ${lng}`);
   }
 }
 
